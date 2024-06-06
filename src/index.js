51e1fecd9a940329cf7235ad2e1a9b86ecd898d5
Brittany Halterman <halterma@gmail.com>
2024-06-05 20:56:18 -0400

    Implement application-level Express middleware

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import 'dotenv/config';
+import { v4 as uuidv4 } from 'uuid';
 import cors from 'cors';
 import express from 'express';
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  req.me = users[1];
+  next();
+});
 
 app.use(cors());
 
@@ -30,6 +39,10 @@ let messages = {
   },
 };
 
+app.get('/session', (req, res) => {
+  return res.send(users[req.me.id]);
+});
+
 app.get('/users', (req, res) => {
   return res.send(Object.values(users));
 });
@@ -46,20 +59,26 @@ app.get('/messages/:messageId', (req, res) => {
   return res.send(messages[req.params.messageId]);
 });
 
-app.post('/users', (req, res) => {
-  return res.send('POST HTTP method on user resource');
+app.post('/messages', (req, res) => {
+  const id = uuidv4();
+  const message = {
+    id,
+    text: req.body.text,
+    userId: req.me.id,
+  };
+
+  messages[id] = message;
+
+  return res.send(message);
 });
 
-app.put('/users/:userId', (req, res) => {
-  return res.send(
-    `PUT HTTP method on user/${req.params.userId} resource`
-  );
-});
+app.delete('/messages/:messageId', (req, res) => {
+  const { [req.params.messageId]: message, ...otherMessages } =
+    messages;
 
-app.delete('/users/:userId', (req, res) => {
-  return res.send(
-    `DELETE HTTP method on user/${req.params.userId} resource`
-  );
+  messages = otherMessages;
+
+  return res.send(message);
 });
 
 app.listen(process.env.PORT, () =>
