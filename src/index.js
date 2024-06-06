ec790c27dd1bcdf66ed046798bb1204be2a9902e
Brittany Halterman <halterma@gmail.com>
2024-06-06 19:30:02 -0400

    Connect PostgreSQL to Express routes with Sequalize

--- src/index.js
+++ src/index.js
@@ -11,10 +11,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
+app.use(async (req, res, next) => {
   req.context = {
     models,
-    me: models.users[1],
+    me: await models.User.findByLogin('rwieruch'),
   };
   next();
 });
